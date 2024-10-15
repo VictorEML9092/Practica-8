b2816ec340c9cb3a970c5d64042ba8662be5fcf5
Victor Mendoza <sonicthedark2000@gmail.com>
2024-10-15 17:48:23 -0600

    Colas_2.py

--- Colas_2.py
+++ Colas_2.py
@@ -3,33 +3,35 @@ Created on Tuesday 15/10/24
 
 @author: Victor Mendoza
 """
-class Cola:
-    def crear_colas(self,clientes):
-        self.Turnos = list(range(1,clientes + 1))
-        self.Servicios = []
+class Cola: # Se crea la clase Cola
+    def crear_colas(self,clientes): # Método para crear las colas
+        self.Turnos = list(range(1,clientes + 1)) # Cola con los turnos del 1 hasta la cantidad de clientes
+        self.Servicios = [] # Cola con los números de servicio
 
-    def dar_turno(self):
-        for i in range(len(self.Turnos)):
+    def dar_turno(self): # Método para dar los turnos
+        for i in range(len(self.Turnos)): # Ciclo para pedir los números de servicio
             self.servicio = input("\nIngrese su número de servicio con 3 dígitos sin contar la letra C (C104): ")
-            self.Servicios.append(self.servicio)
-            print(f"Su número de atención es: {self.Turnos[i]}")
-        print("\nLos números de servicios son:")
-        for num in self.Servicios:
-            print(num)
+            self.Servicios.append(self.servicio) # Se agregan los números de servicio a la cola
+            print(f"Su número de atención es: {self.Turnos[i]}") # Se imprime el turno para el cliente
 
     def atender_cliente(self):
-        self.atencion = input("\nIngrese el número de servicio con 3 dígitos sin contar la letra A (A104): ")
-        self.num_atencion = self.atencion[-3:]
-        self.cliente = next((elem for elem in self.Servicios if elem[-3:] == self.num_atencion), None)
-        print(f"Se atenderá a {self.cliente}")
-        self.Resp = input("\n¿Desea atender a otro cliente?(Si/No) ").lower()
-        if self.Resp == "si":
+        print("\nLos números de servicios son:") # Se imprimen los números de servicio
+        for num in self.Servicios:
+            print(num)
+        self.atencion = input("\nIngrese el número de servicio con 3 dígitos sin contar la letra A (A104): ") # Se pide el número de servicio para atender
+        self.num_atencion = self.atencion[-3:] # Se guardan sólo los dígitos
+        self.cliente = next((elem for elem in self.Servicios if elem[-3:] == self.num_atencion), None) # Se buscan en la lista de Servicios
+        print(f"Se atenderá a {self.cliente}") # Se imprime a quien se atenderá
+        self.Resp = input("\n¿Desea atender a otro cliente?(Si/No) ").lower() # Se pregunta si quieren atender a alguien más
+        if self.Resp == "si": # Concicial if si la respuesta es si, se hace la llama recursiva del método
+            self.Servicios.remove(self.cliente) # Se elimina al cliente atendido de la lista Servicios
             self.atender_cliente()
         else:
-            print("Gracias por su visita, vuelva pronto")
+            print("\nGracias por su visita, vuelva pronto") # Condicional else con mensaje de despedida
 
-S = Cola()
-print("Bienvenido a Seguros Mendoza")
+S = Cola() # Se crea el objeto de la clase Cola
+print("Bienvenido a Seguros Mendoza") # Mensaje de bienvenida
+# Se usan los métodos de la clase
 S.crear_colas(5)
 S.dar_turno()
 S.atender_cliente()
