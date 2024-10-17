92e4ead5aeb4ce9737452c1485c1797542e331e0
Victor Mendoza <sonicthedark2000@gmail.com>
2024-10-16 18:57:34 -0600

    Colas_2.py

--- Colas_2.py
+++ Colas_2.py
@@ -3,35 +3,57 @@ Created on Tuesday 15/10/24
 
 @author: Victor Mendoza
 """
-class Cola: # Se crea la clase Cola
+class Cola: # Se crea la clase base Cola
     def crear_colas(self,clientes): # Método para crear las colas
         self.Turnos = list(range(1,clientes + 1)) # Cola con los turnos del 1 hasta la cantidad de clientes
         self.Servicios = [] # Cola con los números de servicio
 
     def dar_turno(self): # Método para dar los turnos
-        for i in range(len(self.Turnos)): # Ciclo para pedir los números de servicio
-            self.servicio = input("\nIngrese su número de servicio con 3 dígitos sin contar la letra C (C104): ")
-            self.Servicios.append(self.servicio) # Se agregan los números de servicio a la cola
-            print(f"Su número de atención es: {self.Turnos[i]}") # Se imprime el turno para el cliente
+        self.servicio = input("\nIngrese su número de servicio con 3 dígitos sin contar la letra C (C104): ")
+        self.Servicios.append(self.servicio) # Se agregan los números de servicio a la cola
+        print(f"Su número de atención es: {self.Turnos[i]}") # Se imprime el turno para el cliente
 
     def atender_cliente(self):
         print("\nLos números de servicios son:") # Se imprimen los números de servicio
         for num in self.Servicios:
             print(num)
+
         self.atencion = input("\nIngrese el número de servicio con 3 dígitos sin contar la letra A (A104): ") # Se pide el número de servicio para atender
         self.num_atencion = self.atencion[-3:] # Se guardan sólo los dígitos
-        self.cliente = next((elem for elem in self.Servicios if elem[-3:] == self.num_atencion), None) # Se buscan en la lista de Servicios
-        print(f"Se atenderá a {self.cliente}") # Se imprime a quien se atenderá
-        self.Resp = input("\n¿Desea atender a otro cliente?(Si/No) ").lower() # Se pregunta si quieren atender a alguien más
-        if self.Resp == "si": # Concicial if si la respuesta es si, se hace la llama recursiva del método
-            self.Servicios.remove(self.cliente) # Se elimina al cliente atendido de la lista Servicios
+        while self.num_atencion != self.Servicios[0][-3:]: # Ciclo para comprobar que atienda al primero en la cola
+            print("Debe ingresar el número de servicio que esta primero en la cola")
+            self.atencion = input("\nIngrese el número de servicio con 3 dígitos sin contar la letra A (A104): ")
+            self.num_atencion = self.atencion[-3:]
+
+        cliente = self.Servicios.pop(0) # Se elimina de la cola al cliente que se atenderá
+        print(f"Se atenderá a {cliente}") # Se imprime a quien se atenderá
+
+        if self.Servicios: # Concicial if si la cola tiene clientes, se hace la llama recursiva del método
             self.atender_cliente()
         else:
-            print("\nGracias por su visita, vuelva pronto") # Condicional else con mensaje de despedida
+            print("\nYa no quedan clientes para atender") # Condicional else con mensaje para avisar que ya no hay clientes
 
-S = Cola() # Se crea el objeto de la clase Cola
+class Servicios(Cola): # Se crea la clase variante
+    def servicio1(self): # Método para el primer servicio
+        super().dar_turno() # Se usa el método heredado
+
+    def servicio2(self): # Método para el primer servicio
+        super().dar_turno() # Se usa el método heredado
+
+    def servicio3(self): # Método para el primer servicio
+        super().dar_turno() # Se usa el método heredado
+
+S = Servicios() # Objeto de la clase variante
+
 print("Bienvenido a Seguros Mendoza") # Mensaje de bienvenida
-# Se usan los métodos de la clase
-S.crear_colas(5)
-S.dar_turno()
-S.atender_cliente()
+Clientes = int(input("\nIngrese la cantidad de clientes: ")) # Se pide la cantidad de clientes
+S.crear_colas(Clientes) # Se usa el método de la clase base
+for i in range(Clientes): # Ciclo para preguntar que servicio necesita el cliente
+    Resp = input("\n¿Que servicio desea usar? (Vida, Vehículos, Médico) ").lower()
+    if Resp == "vida":
+        S.servicio1() # Se usa el método de la clase variante
+    elif Resp == "vehículos":
+        S.servicio2() # Se usa el método de la clase variante
+    else:
+        S.servicio3() # Se usa el método de la clase variante
+S.atender_cliente() # Se usa el método de la clase base
